087e6a8e2946df63ff98eebb6f4ea8b9821e427a
idcm <idcm@users.noreply.github.com>
2020-08-22 02:52:30 +0800

    Update Types.hs

--- src/DFINITY/Discovery/Types.hs
+++ src/DFINITY/Discovery/Types.hs
@@ -182,8 +182,7 @@ toByteStruct
 fromByteStruct
   :: ByteStruct
   -> Ident
-fromByteStruct bs
-  = Ident $ vectorToByteString . convert . cloneToWords8
+fromByteStruct = Ident $ vectorToByteString . convert . cloneToWords8
 
 --------------------------------------------------------------------------------
 
