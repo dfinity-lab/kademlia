c9f77c6f1637093e5090c9d03aa8d8eab97b4361
idcm <idcm@users.noreply.github.com>
2020-08-23 18:58:31 +0800

    Update Types.hs

--- test/Tests/Types.hs
+++ test/Tests/Types.hs
@@ -16,16 +16,21 @@ module Tests.Types
 --------------------------------------------------------------------------------
 
 import           Test.QuickCheck         ()
-
+import           Data.Bits               (testBit)
 import           DFINITY.Discovery.Types
                  (Ident (..), fromByteStruct, toByteStruct)
-
+import           Data.Bit.ThreadSafe     (unBit)
+import           Data.Vector.Unboxed     (length, (!))
+import           Prelude                 hiding (length)
 --------------------------------------------------------------------------------
 
 -- | Checks whether toByteStruct converts correctly
 toByteStructCheck :: Ident -> Bool
--- | FIXME
-toByteStructCheck _ = True
+toByteStructCheck nid = foldl foldingFunc True [0..length converted - 1]
+    where converted = toByteStruct nid
+          byteWords = B.unpack . fromIdent $ nid
+          foldingFunc b i = b && (unBit (converted ! i) == access byteWords i)
+          access ws i = testBit (ws !! (i `div` 8)) (i `mod` 8)
 
 -- | Checks whether fromByteStruct converts correctly
 fromByteStructCheck :: Ident -> Bool
